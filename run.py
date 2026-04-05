361097564972c3631ce6c6f004ef38c2c5db8fdb
Ben Cordes <cordes.ben@gmail.com>
2026-04-05 01:42:58 -0400

    tz-aware dates, bugfixes

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from datetime import datetime
+from datetime import datetime, UTC
 from pathlib import Path
 
 from cryptography.x509 import load_pem_x509_certificate
@@ -9,7 +9,7 @@ from requests import Session
 BASE_URL = "https://kernighan.hitchhikerprod.com/api/states/sensor.certificate"
 
 with open("/root/.hassio-key", "rt", encoding="utf-8") as key_file:
-    bearer_token = key_file.read()
+    bearer_token = key_file.read().strip("\n")
 
 session = Session()
 session.headers.update({
@@ -23,15 +23,15 @@ for dirname in Path("/etc/letsencrypt/live").glob("*.com"):
     with open(dirname / "cert.pem", "rb") as cert_file:
         cert = load_pem_x509_certificate(cert_file.read())
 
-    valid = cert.not_valid_before <= datetime.now() <= cert.not_valid_after
+    valid = cert.not_valid_before_utc <= datetime.now(UTC) <= cert.not_valid_after_utc
     body = {
         "state": valid,
         "attributes": {
-            "not_valid_before": cert.not_valid_before.isoformat(),
-            "not_valid_after": cert.not_valid_after.isoformat()
+            "not_valid_before": cert.not_valid_before_utc.isoformat(),
+            "not_valid_after": cert.not_valid_after_utc.isoformat()
         }
     }
 
-    response = session.post(f"{BASE_URL}_{sensor_name}")
+    response = session.post(f"{BASE_URL}_{sensor_name}", json=body)
     if not response.ok:
         print(f"Error setting {sensor_name}")
